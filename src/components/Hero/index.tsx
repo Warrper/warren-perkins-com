f25361149cbfc4e5cd296ac27bcfde5a0a7a0d50
Warrper <17754468+Warrper@users.noreply.github.com>
2022-03-22 01:02:13 +0000

    show animation only on first load using session storage

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,16 +3,18 @@ import { HeroWrapper } from './index.css';
 import TypeWriter from '../TypeWriter';
 import useMediaQuery from '../../hooks/useMediaQuery';
 
-interface HeroProps {}
+interface HeroProps {
+    animate: boolean;
+}
 
-const Hero: FunctionalComponent<HeroProps> = () => {
+const Hero: FunctionalComponent<HeroProps> = ({ animate }) => {
     const isMobile = useMediaQuery('(max-width: 600px)');
     return (
         <HeroWrapper>
             <TypeWriter
                 fontSize={isMobile ? '3rem' : '5rem'}
                 text={"Hi, I'm"}
-                typeSpeed={600}
+                typeSpeed={animate ? 600 : 0.0001}
                 delayStart={0}
                 noBlink={true}
             />
@@ -20,15 +22,15 @@ const Hero: FunctionalComponent<HeroProps> = () => {
                 fontSize={isMobile ? '3rem' : '5rem'}
                 text={'Warren Perkins'}
                 color={'#00EDFF'}
-                typeSpeed={1000}
-                delayStart={700}
+                typeSpeed={animate ? 1000 : 0.0001}
+                delayStart={animate ? 700 : 0}
                 noBlink={true}
             />
             <TypeWriter
                 fontSize={isMobile ? '1rem' : '2rem'}
                 text={`I'm a developer.`}
-                typeSpeed={1000}
-                delayStart={700 + 1500}
+                typeSpeed={animate ? 1000 : 0.0001}
+                delayStart={animate ? 2200 : 0}
             />
         </HeroWrapper>
     );
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,7 +1,9 @@
 import { FunctionalComponent } from 'preact';
+import { useEffect } from 'react';
 import { HomeWrapper } from './index.css';
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
+import { browserStore } from '../../utils/browserStore';
 // import ArticleCard from '../../components/ArticleCard';
 // import { CardWrapper } from '../../components/util.css';
 import LargeCard from '../../components/LargeCard';
@@ -9,10 +11,16 @@ import LargeCard from '../../components/LargeCard';
 interface HomeProps {}
 
 const Home: FunctionalComponent<HomeProps> = () => {
+    const hasSeenAnimation = browserStore.get('sessionStorage', 'hasSeenAnimation');
+
+    useEffect(() => {
+        browserStore.set('sessionStorage', 'hasSeenAnimation', 'true');
+    }, []);
+
     return (
         <HomeWrapper>
             <Header />
-            <Hero />
+            <Hero animate={!hasSeenAnimation} />
 
             <LargeCard
                 title="Frontend"
@@ -20,12 +28,12 @@ const Home: FunctionalComponent<HomeProps> = () => {
                 totam maiores fugit quibusdam aperiam facere! Dolores iste numquam quidem quaerat. Assumenda, laboriosam 
                 magnam hic suscipit eaque omnis ut vel sunt eos. Harum molestias dignissimos minima odio impedit consectetur, 
                 vero soluta maiores unde nam nobis sed cumque perspiciatis.`}
-                delay={3500}
+                delay={hasSeenAnimation ? 0 : 3500}
             />
             <LargeCard
                 title="Backend"
                 content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa modi dignissimos repellendus, totam maiores fugit quibusdam aperiam facere! Dolores iste numquam quidem quaerat. Assumenda, laboriosam magnam hic suscipit eaque omnis ut vel sunt eos. Harum molestias dignissimos minima odio impedit consectetur, vero soluta maiores unde nam nobis sed cumque perspiciatis."
-                delay={4000}
+                delay={hasSeenAnimation ? 500 : 4000}
             />
         </HomeWrapper>
     );
