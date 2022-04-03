b38ac6a523547982a81571b0cb3d6d03f5bf5a4c
Warrper <17754468+Warrper@users.noreply.github.com>
2022-04-03 12:05:22 +0100

    feat: Metadata and api utils - adds robots.txt for SEO - adds meta-image for twitter and facebook - adds api util functions

---
+++ src/utils/api.ts
@@ -0,0 +1,38 @@
+import { siteUrl } from "../constants";
+
+interface BlogPostMetadata {
+    title: string;
+    description: string;
+    created: string;
+    slug: string;
+    content: string;
+    tags: string[];
+}
+
+interface BlogBaseApi {
+    tags: string[];
+    tagsLocation: string;
+    posts: string[];
+    postsLocation: string;
+}
+
+async function fetchBlogBase(): Promise<BlogBaseApi> {
+    const res = await fetch(`${siteUrl}/api/blog/index.json`).then((res) => res.json());
+    return res;
+}
+
+async function fetchBlogPostMetadata(blogBase: BlogBaseApi, slug: string): Promise<BlogPostMetadata> {
+    const res = await fetch(`${siteUrl}${blogBase.postsLocation}/${slug}.json`).then((res) => res.json());
+    return res;
+}
+
+async function fetchBlogTag(blogBase: BlogBaseApi, slug: string): Promise<string[]> {
+    const res = await fetch(`${siteUrl}${blogBase.tagsLocation}/${slug}.json`).then((res) => res.json());
+    return res;
+}
+
+export default {
+    fetchBlogBase,
+    fetchBlogPostMetadata,
+    fetchBlogTag
+}
