e8c06f374f79a96cde71ddc5d571df3fe2429c0b
Warrper <17754468+Warrper@users.noreply.github.com>
2022-04-24 21:29:31 +0100

    infra: begin work on pulumi IAC - Create Lambda function with remix code - Create http api gateway to serve lamba

---
+++ REMIX/warren-perkins-com/infrastructure/index.ts
@@ -0,0 +1,22 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as lambda from './lambda';
+import * as apiGateway from './apiGateway';
+
+(async () => {
+    const projectName = 'wp-com';
+    const cId = await aws.getCallerIdentity({});
+
+    const remixFn = await lambda.callbackFunction({
+        name: `${projectName}-remix-lambda`,
+        code: '../server/index.js',
+        cId,
+    });
+
+    const apigw = await apiGateway.httpGateway({
+        name: `${projectName}-api-gateway`,
+        functionArn: remixFn.arn,
+        cId,
+    })
+
+})();
---
+++ REMIX/warren-perkins-com/infrastructure/lambda/index.ts
@@ -0,0 +1,1 @@
+export * from './callbackFunction';
