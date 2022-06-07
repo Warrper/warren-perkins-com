93ebf9844b8d39b35d35073c07ea215f24c309b0
Warrper <17754468+Warrper@users.noreply.github.com>
2022-06-07 21:45:13 +0100

    chore: update site metadata

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,14 @@ export const links: LinksFunction = () => [
         href: 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;800&family=Lato&display=swap',
     },
     {
+        rel: 'icon',
+        href: 'https://warrenperkins.com/favicon.ico',
+    },
+    {
+        rel: 'icon',
+        href: 'https://warrenperkins.com/_static/site-assets/favicon.svg',
+    },
+    {
         rel: 'stylesheet',
         href: tailwindStyles,
     },
@@ -32,6 +40,15 @@ export const meta: MetaFunction = () => ({
     charset: 'utf-8',
     title: 'Warren Perkins',
     viewport: 'width=device-width,initial-scale=1',
+    description: `I'm a full stack web developer from the north of england.`,
+    'og:type': 'website',
+    'og:title': 'Warren Perkins',
+    'og:description': `I'm a full stack web developer from the north of england.`,
+    'og:image': 'https://warrenperkins.com/_static/site-assets/meta-image.png',
+    'twitter:card': 'summary_large_image',
+    'twitter:title': 'Warren Perkins',
+    'twitter:description': `I'm a full stack web developer from the north of england.`,
+    'twitter:image': 'https://warrenperkins.com/_static/site-assets/meta-image.png',
 });
 
 export default function App() {
