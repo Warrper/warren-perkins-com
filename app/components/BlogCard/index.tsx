9f8f3fa93b390248d8adecae7c5ecce2cfea8882
Warrper <17754468+Warrper@users.noreply.github.com>
2022-06-12 23:11:57 +0100

    blog: rust-wasm-speed-comparison - Updates BlogCard to work with longer titles - Updates BlogContent to center images - creates rust-wasm-speed-comparison blog post

--- app/components/BlogCard/index.tsx
+++ app/components/BlogCard/index.tsx
@@ -14,18 +14,21 @@ export default function BlogCard({ title, description, href, image }: BlogCardPr
             className={`group m-1 my-4 flex h-96 w-11/12 flex-col overflow-hidden rounded-md bg-[length:auto_100%] bg-center transition-all duration-700 hover:bg-[length:auto_150%] md:w-5/12 lg:w-3/12 shadow-xl`}
             style={{ backgroundImage: `url(${image ? image : 'https://picsum.photos/1920/1080'})` }}
         >
-            <div className="h-full w-full bg-[rgba(0,0,0,0.5)] p-6">
-                <h1 className="text-shadow-md h-1/2 text-2xl text-white transition-all duration-500 group-hover:mb-0 group-hover:h-1/2 group-hover:pt-0 md:mb-10 md:h-full md:pt-72 md:text-3xl">
-                    {title}
-                </h1>
-                <p className="font-bold text-white text-md lg:text-xl text-shadow-md">{description}</p>
-                <div className="mt-8 md:mt-8">
-                    <Link
-                        to={href}
-                        className="text-shadow-md font-bold w-32 p-3 md:p-2 border border-neon-blue text-center hover:bg-neon-blue rounded-md"
-                    >
-                        Read More
-                    </Link>
+            <div className="h-full w-full bg-[rgba(0,0,0,0.5)] text-white">
+                <div className="flex flex-col h-1/2 md:h-full group-hover:h-1/2 transition-all duration-500">
+                    <div className='flex flex-grow md:group-hover:flex-grow-0 transition-all duration-700'></div>
+                    <h1 className='font-bold text-shadow-md text-3xl md:text-3xl text-white px-2 py-5'>{title}</h1>
+                </div>
+                <div className="flex h-1/2 flex-col">
+                    <p className=" text-white text-md text-shadow-md px-4">{description}</p>
+                    <div className="mt-8 md:mt-8 px-4">
+                        <Link
+                            to={href}
+                            className="text-shadow-md font-bold w-32 p-3 md:p-2 border border-neon-blue text-center hover:bg-neon-blue rounded-md"
+                        >
+                            Read More
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
