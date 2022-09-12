af3788cb138f5431efa76fbc1bc59b01dd2781b3
Warrper <17754468+Warrper@users.noreply.github.com>
2022-09-12 22:30:55 +0100

    chore: update client and server entry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,30 +1,64 @@
-import type { EntryContext } from '@remix-run/node';
-import { RemixServer } from '@remix-run/react';
-import { renderToString } from 'react-dom/server';
+import { PassThrough } from "stream";
+import type { EntryContext } from "@remix-run/node";
+import { Response } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import isbot from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
-    request: Request,
-    responseStatusCode: number,
-    responseHeaders: Headers,
-    remixContext: EntryContext
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
 ) {
-    let markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
+  const callbackName = isbot(request.headers.get("user-agent"))
+    ? "onAllReady"
+    : "onShellReady";
 
-    responseHeaders.set('Content-Type', 'text/html');
-    responseHeaders.set('Strict-Transport-Security', "31536000");
-    responseHeaders.set('X-Frame-Options', 'SAMEORIGIN');
-    responseHeaders.set('Referrer-Policy', "strict-origin");
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-    const cacheControl = responseHeaders.get('Cache-Control');
-    if (!cacheControl) {
-        responseHeaders.set(
-            'Cache-Control',
-            'public, max-age=86400, s-maxage=2628000, stale-while-revalidate=31560000'
-        );
-    }
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        [callbackName]: () => {
+          const body = new PassThrough();
+          
+          responseHeaders.set('Content-Type', 'text/html');
+          responseHeaders.set('Strict-Transport-Security', "31536000");
+          responseHeaders.set('X-Frame-Options', 'SAMEORIGIN');
+          responseHeaders.set('Referrer-Policy', "strict-origin");
 
-    return new Response('<!DOCTYPE html>' + markup, {
-        status: responseStatusCode,
-        headers: responseHeaders,
-    });
+          const cacheControl = responseHeaders.get('Cache-Control');
+          if (!cacheControl) {
+              responseHeaders.set(
+                  'Cache-Control',
+                  'public, max-age=86400, s-maxage=2628000, stale-while-revalidate=31560000'
+              );
+          }
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError: (err: unknown) => {
+          reject(err);
+        },
+        onError: (error: unknown) => {
+          didError = true;
+
+          console.error(error);
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
 }
